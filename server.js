3fbcaabd660ee863c056bfac031941b19185b81e
mnbarber <mnbarber1993@gmail.com>
2022-04-26 10:49:38 -0700

    views folder and models folder up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const { default: mongoose } = require('mongoose');
 const app = express();
 const PORT = 4000;
 
@@ -7,6 +8,17 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-
+// const gameSchema = new mongoose.Schema({
+//     title: {
+//         type: String,
+//         required: true
+//     },
+//     release: {
+//         type: String,
+//         required: true
+//     },
+//     image: String,
+//     price: Number
+// })
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
