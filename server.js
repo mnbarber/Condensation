1886ebd33bf10de8bdfce827d9e9a86de2da1962
mnbarber <mnbarber1993@gmail.com>
2022-04-26 15:50:53 -0700

    all connections are working

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const methodOverride = require('method-override');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
 const gameController = require('./controllers/game_controller');
 const app = express();
 const PORT = 4000;
+
+require('./config/db.connection');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
