b261b47e0503ebe72d46454612387864788890a1
Jonathan <jonathankimwylie@gmail.com>
2022-04-29 10:33:37 -0500

    local changes to port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const methodOverride = require('method-override');
 const controllers = require('./controllers')
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 require('./config/db.connection');
 
